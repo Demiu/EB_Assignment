86f630e2e27e37d611194825a90548cb26852da4
grudos <jg04@wp.pl>
2021-11-11 12:56:46 +0100

    Adding scraper 2

--- scripts/scraper/Lamp.py
+++ scripts/scraper/Lamp.py
@@ -1,5 +1,5 @@
 class Lamp:
-    def __init__(self, id, name, category, price, delivery, producer, amount, technicalData, description, url, image_lamp, logo):
+    def __init__(self, id, name, category, price, delivery, producer, amount, technicalData, description, url, image_lamp, producer_logo):
         self.id = id
         self.name = name
         self.category = category
@@ -11,13 +11,13 @@ class Lamp:
         self.description = description
         self.url = url
         self.image_lamp = image_lamp
-        self.logo = logo
+        self.producer_logo = producer_logo
 
-# BYC MOZE PO SELF PRODUCER TRZEBA DODAC 1 ; DLA MPN, RACZEJ NIE ALE NA RAZIE JEST DODANE
-    def convert_to_CSV(self):
-        inf = str(self.id) + ";1;" + self.name + ";" + self.category + ";" + self.price + ";1;;0;;;;;;;;"
-        inf += self.producer + ";;;;;;;;;" + str(self.delivery) + ";;" + str(self.amount) + ";1;1;1;both;;;;" + str(self.technicalData) + ";"
-        inf += str(self.description) + ";" + self.producer + ";Meta title-" + str(self.id) + ";Meta keywords-" + str(self.id) + ";Meta description-" + str(self.id) + ";"
-        inf += self.url + ";Dostępny;Niedostępny;1;;;1;" + self.image_lamp + ", " + self.logo + ";;0;;0;new;0;0;0;0;0;;;;;0;0;0;0;"
+# JEDEN ; BYC MOZE DO WYRZUCENIA PO SELF PRODUCER BO MOZE MPN JEST NIEPOTRZEBNE
+    def convertToCSV(self):
+        answer = str(self.id) + ";1;" + self.name + ";" + self.category + ";" + self.price + ";1;;0;;;;;;;;"
+        answer += self.producer + ";;;;;;;;;" + str(self.delivery) + ";;" + str(self.amount) + ";1;1;1;both;;;;" + str(self.technicalData) + ";"
+        answer += str(self.description) + ";" + self.producer + ";Meta title-" + str(self.id) + ";Meta keywords-" + str(self.id) + ";Meta description-" + str(self.id) + ";"
+        answer += self.url + ";Dostępny;Niedostępny;1;;;1;" + self.image_lamp + ", " + self.producer_logo + ";;0;;0;new;0;0;0;0;0;;;;;0;0;0;0;"
 
-        return inf
+        return answer
--- scripts/scraper/Scraper.py
+++ scripts/scraper/Scraper.py
@@ -3,133 +3,178 @@ import requests
 
 from Lamp import Lamp
 from Page import Page
+from Combination import Combination
+
+pages_urls = ["11-lampy-sufitowe-plafony", "10-lampy-wiszace-zyrandole", "13-lampy-scienne-kinkiety",
+              "14-lampki-stolowe-biurkowe", "15-lampy-podlogowe", "21-lampy-zewnetrzne-sufitowe",
+              "22-lampy-zewnetrzne-wiszace", "23-lampy-scienne-elewacyjne", "153-lampy-stojace-slupki",
+              "24-lampy-masztowe-latarnie", "4-zarowki-zrodla-swiatla"]
+pages_categories = ["Lampy sufitowe | plafony", "Lampy wiszące | żyrandole", "Lampy ścienne | kinkiety",
+                   "Lampki stołowe | biurkowe", "Lampy stojące podłogowe", "Lampy zewnętrzne sufitowe",
+                   "Lampy zewnętrzne wiszące", "Kinkiety ścienne i oświetlenie elewacyjne",
+                   "Lampy stojące ogrodowe i słupki ogrodowe", "Latarnie ogrodowe i dekoracyjne lampy masztowe",
+                   "Żarówki"]
 
 
-def select_pages():
+def selectPages():
     pages = []
-    pages.append(Page("11-lampy-sufitowe-plafony", "Lampy sufitowe | plafony"))
-    pages.append(Page("10-lampy-wiszace-zyrandole", "Lampy wiszące | żyrandole"))
-    pages.append(Page("13-lampy-scienne-kinkiety", "Lampy ścienne | kinkiety"))
-    pages.append(Page("14-lampki-stolowe-biurkowe", "Lampki stołowe | biurkowe"))
-    pages.append(Page("15-lampy-podlogowe", "Lampy stojące podłogowe"))
-    pages.append(Page("21-lampy-zewnetrzne-sufitowe", "Lampy zewnętrzne sufitowe"))
-    pages.append(Page("22-lampy-zewnetrzne-wiszace", "Lampy zewnętrzne wiszące"))
-    pages.append(Page("23-lampy-scienne-elewacyjne", "Kinkiety ścienne i oświetlenie elewacyjne"))
-    pages.append(Page("23-lampy-scienne-elewacyjne", "Kinkiety ścienne i oświetlenie elewacyjne"))
-    pages.append(Page("153-lampy-stojace-slupki", "Lampy stojące ogrodowe i słupki ogrodowe"))
-    pages.append(Page("24-lampy-masztowe-latarnie", "Latarnie ogrodowe i dekoracyjne lampy masztowe"))
-    pages.append(Page("4-zarowki-zrodla-swiatla", "Żarówki"))
+    for i in range(len(pages_urls)):
+        pages.append(Page(pages_urls[i], pages_categories[i]))
     return pages
 
-def get_content(url, prodID):
+
+def getContent(url, prod_id):
     page = requests.get(url)
     content = page.text
     soup = BeautifulSoup(content, "html.parser")
-    products = soup.find(id = prodID)
+    products = soup.find(id=prod_id)
     return products
 
-def get_description(productInfo):
+
+def getDescription(productInfo):
     header = productInfo.find("h2").text
-    #answer = "<h2>" + header + "</h2>\n<tbody>\n<tr>\n<td>\n"
     answer = "<h2>" + header + "</h2><tbody><tr><td>"
     text = productInfo.find_all("p")
+
     for p in text:
-        #answer += "<p>" + p.text + "</p>\n"
         answer += "<p>" + p.text + "</p>"
 
-    #answer += ("</td>\n<td width=510></td>\n<tr>\n</tbody>\n")
     answer += ("</td><td width=510></td><tr></tbody>")
-    #print(answer)
     return answer
 
-def get_technicalData(productInfo):
+
+def getTechnicalData(productInfo):
     header = productInfo.find("h3").text
-    #answer = "<h3>" + header + "</h3>\n<dl>\n"
     answer = "<h3>" + header + "</h3><dl>"
     dts = productInfo.find_all("dt")
     dds = productInfo.find_all("dd")
+
     for i in range(len(dts)):
-        #print(dts[i].text)
-        #print(dds[i].text)
-        #answer += "<dt>" + dts[i].text.replace("\n", " ") + "</dt>\n"
         answer += "<dt>" + dts[i].text.replace("\n", " ") + "</dt>"
-        #answer += "<dd>" + dds[i].text.replace("\n", " ") + "</dd>\n"
         answer += "<dd>" + dds[i].text.replace("\n", " ") + "</dd>"
 
     answer += "</dl>"
-    #print(answer)
     return answer
 
 
-mainUrl = "https://mlamp.pl/"
-pages = select_pages()
-productsIdInUrl = "js-product-list"
-productSubstring = "js-product-miniature-wrapper"
+def getLampInformations(main, section_product):
+    lamp_name = main.find("h1").text
+
+    lamp_info = section_product.find('div', class_='product-lmage-large')
+    image_lamp = lamp_info.find('img')['src']
+
+    producer_info = section_product.find('div', class_='product-manufacturer product-manufacturer-next float-right')
+    producer_logo = producer_info.find('img')['src']
+    producer = producer_info.find('img')['alt']
+
+    price = section_product.find("span", class_="product-price").text[:-3]
+    delivery = section_product.find("span", class_="product-available").text.strip()
+
+    amount = "0"
+
+    try:
+        product_quantities = section_product.find('div', class_='product-quantities')
+        amount = product_quantities.find('span')['data-stock']
+    except:
+        # if amount = "0" then we do not take lamps which are not there yet
+        pass
+
+    return lamp_name, image_lamp, producer_logo, producer, price, delivery, amount
+
+
+def createProductsCombinations(id_start_combinations, id_end_combinations):
+    file = open('data/combinations.csv', 'w', encoding='utf-8')
+    file.write("ID;Indeks produktu;Atrybut (Nazwa:Typ:Pozycja)*;Wartość (Wartość:Pozycja)*;Identyfikator dostawcy;Indeks;\
+EAN13;UPC;MPN;Koszt własny;Wpływ na cenę;Podatek ekologiczny;Ilość;Minimalna ilość;Niski poziom produktów w magazynie;\
+Wyślij do mnie e-mail, gdy ilość jest poniżej tego poziomu;Wpływ na wagę;Domyślny (0 = Nie, 1 = Tak);Data dostępności\
+kombinacji;Wybierz z pośród zdjęć produktów wg pozycji (1,2,3...);Adresy URL zdjęcia (x,y,z...);Tekst alternatywny dla\
+zdjęć (x,y,z...);ID / Nazwa sklepu;Zaawansowane zarządzanie magazynem;Zależny od stanu magazynowego;Magazyn")
+    file.close()
+
+    for i in range(len(id_start_combinations)):
+        for j in range(id_start_combinations[i], id_end_combinations[i] + 1):
+            combination = Combination(j, True)
+            combination = "\n" + combination.convertToCSV()
+            with open('data/combinations.csv', 'a', encoding='utf-8') as file:
+                file.write(combination)
+            combination = Combination(j, False)
+            combination = "\n" + combination.convertToCSV()
+            with open('data/combinations.csv', 'a', encoding='utf-8') as file:
+                file.write(combination)
+
+    file.close()
+
+
+site_url = "https://mlamp.pl/"
+pages = selectPages()
+products_id_url = "js-product-list"
+product_substring = "js-product-miniature-wrapper"
 id = 0
 
-with open('data/productsInCSV.csv', 'w', encoding='utf-8') as file:
-    file.write("ID;Aktywny (0 lub 1);Nazwa;Kategorie (x,y,z…);Cena zawiera podatek. (brutto);ID reguły podatku;Koszt własny;W sprzedaży (0 lub 1);Wartość rabatu;Procent rabatu;Rabat od dnia (rrrr-mm-dd);Rabat do dnia (rrrr-mm-dd);Indeks #;Kod dostawcy #;Dostawca;Marka;EAN13;UPC;Ecotax;Szerokość;Wysokość;Głębokość;Waga;Czas dostawy produktów dostępnych w magazynie:;Czas dostawy wyprzedanych produktów z możliwością rezerwacji:;Ilość;Minimalna ilość;Niski poziom produktów w magazynie;Wyślij do mnie e-mail, gdy ilość jest poniżej tego poziomu;Widoczność;Dodatkowe koszty przesyłki;Jednostka dla ceny za jednostkę;Cena za jednostkę;Podsumowanie;Opis;Tagi (x,y,z…);Meta-tytuł;Słowa kluczowe meta;Opis meta;Przepisany URL;Etykieta, gdy w magazynie;Etykieta kiedy dozwolone ponowne zamówienie;Dostępne do zamówienia (0 = Nie, 1 = Tak);Data dostępności produktu;Data wytworzenia produktu;Pokaż cenę (0 = Nie, 1 = Tak);Adresy URL zdjęcia (x,y,z…);Tekst alternatywny dla zdjęć (x,y,z…);Usuń istniejące zdjęcia (0 = Nie, 1 = Tak);Cecha(Nazwa:Wartość:Pozycja:Indywidualne);Dostępne tylko online (0 = Nie, 1 = Tak);Stan:;Konfigurowalny (0 = Nie, 1 = Tak);Można wgrywać pliki (0 = Nie, 1 = Tak);Pola tekstowe (0 = Nie, 1 = Tak);Akcja kiedy brak na stanie;Wirtualny produkt (0 = No, 1 = Yes);Adres URL pliku;Ilość dozwolonych pobrań;Data wygaśnięcia (rrrr-mm-dd);Liczba dni;ID / Nazwa sklepu;Zaawansowane zarządzanie magazynem;Zależny od stanu magazynowego;Magazyn;Akcesoria (x,y,z…)")
+with open('data/products.csv', 'w', encoding='utf-8') as file:
+    file.write("ID;Aktywny (0 lub 1);Nazwa;Kategorie (x,y,z…);Cena zawiera podatek. (brutto);ID reguły podatku;\
+Koszt własny;W sprzedaży (0 lub 1);Wartość rabatu;Procent rabatu;Rabat od dnia (rrrr-mm-dd);Rabat do dnia \
+(rrrr-mm-dd);Indeks #;Kod dostawcy #;Dostawca;Marka;EAN13;UPC;MPN;Ecotax;Szerokość;Wysokość;Głębokość;Waga;\
+Czas dostawy produktów dostępnych w magazynie:;Czas dostawy wyprzedanych produktów z możliwością rezerwacji:;\
+Ilość;Minimalna ilość;Niski poziom produktów w magazynie;Wyślij do mnie e-mail, gdy ilość jest poniżej tego \
+poziomu;Widoczność;Dodatkowe koszty przesyłki;Jednostka dla ceny za jednostkę;Cena za jednostkę;Podsumowanie;Opis;\
+Tagi (x,y,z…);Meta-tytuł;Słowa kluczowe meta;Opis meta;Przepisany URL;Etykieta, gdy w magazynie;Etykieta kiedy \
+dozwolone ponowne zamówienie;Dostępne do zamówienia (0 = Nie, 1 = Tak);Data dostępności produktu;Data wytworzenia \
+produktu;Pokaż cenę (0 = Nie, 1 = Tak);Adresy URL zdjęcia (x,y,z…);Tekst alternatywny dla zdjęć (x,y,z…);Usuń \
+istniejące zdjęcia (0 = Nie, 1 = Tak);Cecha(Nazwa:Wartość:Pozycja:Indywidualne);Dostępne tylko online (0 = Nie, \
+1 = Tak);Stan:;Konfigurowalny (0 = Nie, 1 = Tak);Można wgrywać pliki (0 = Nie, 1 = Tak);Pola tekstowe (0 = Nie, \
+1 = Tak);Akcja kiedy brak na stanie;Wirtualny produkt (0 = No, 1 = Yes);Adres URL pliku;Ilość dozwolonych pobrań;\
+Data wygaśnięcia (rrrr-mm-dd);Liczba dni;ID / Nazwa sklepu;Zaawansowane zarządzanie magazynem;Zależny od stanu \
+magazynowego;Magazyn;Akcesoria (x,y,z…)")
 file.close()
+
+id_start_combinations = []
+id_end_combinations = []
 
 for page in pages:
     url = page.url
     category = page.category
-    products = get_content(mainUrl + url, productsIdInUrl)
+    products = getContent(site_url + url, products_id_url)
 
-    results = products.find_all("div", lambda value: value and value.startswith(productSubstring))
-    for result in results:
-        save = True
+    if page.category == "Lampy sufitowe | plafony":
+        id_start_combinations.append(id + 1)
+        id_end_combinations.append(id + 1)
 
+    if page.category == "Lampy zewnętrzne sufitowe":
+        id_start_combinations.append(id + 1)
+        id_end_combinations.append(id + 1)
+
+    page_results = products.find_all("div", lambda value: value and value.startswith(product_substring))
+
+    for result in page_results:
         a_href = result.find("a").attrs['href']
-        #print(a_href)
 
         a_link = requests.get(a_href)
         content = a_link.text
         soup = BeautifulSoup(content, "html.parser")
+
         main = soup.find("main")
-
-        lamp_name = main.find("h1").text
-        #print(lamp_name)
-
         section_product = main.find("section")
 
-        lamp_info = section_product.find('div', class_='product-lmage-large')
-        image_lamp = lamp_info.find('img')['src']
-
-        #print("image_lamp = " + str(image_lamp))
-
-        producer_info = section_product.find('div', class_='product-manufacturer product-manufacturer-next float-right')
-        producer_logo = producer_info.find('img')['src']
-        producer = producer_info.find('img')['alt']
-
-        #print("producer_logo = " + str(producer_logo))
-        #print("producer = " + str(producer))
-
-        price = section_product.find("span", class_="product-price").text[:-3]
-        #print(price)
-
-        delivery = section_product.find("span", class_="product-available").text.strip()
-        #print(delivery)
+        lamp_name, image_lamp, producer_logo, producer, price, delivery, amount = getLampInformations(main, section_product)
 
         cards = section_product.find_all("div", class_="card")
-        #cards[0] for description, cards[1] for technicalData
-        description = get_description(cards[0])
-        technicalData = get_technicalData(cards[1])
-
-        amount = "0"
-
-        try:
-            product_quantities = section_product.find('div', class_='product-quantities')
-            amount = product_quantities.find('span')['data-stock']
-        except:
-            # if amount = "0" then we do not take lamps which are not there yet
-            pass
+        description = getDescription(cards[0])
+        technicalData = getTechnicalData(cards[1])
 
         if amount != "0":
             id += 1
-            #print("id = " + str(id))
             lamp = Lamp(id, lamp_name, category, price, delivery, producer, amount, technicalData, description, url, image_lamp, producer_logo)
-            product = "\n" + lamp.convert_to_CSV()
-            with open('data/productsInCSV.csv', 'a', encoding='utf-8') as file:
+            product = "\n" + lamp.convertToCSV()
+            with open('data/products.csv', 'a', encoding='utf-8') as file:
                 file.write(product)
+
+            if page.category == "Lampy sufitowe | plafony":
+                id_end_combinations[0] = id
+
+            if page.category == "Lampy zewnętrzne sufitowe":
+                id_end_combinations[1] = id
+
     file.close()
+
+createProductsCombinations(id_start_combinations, id_end_combinations)
