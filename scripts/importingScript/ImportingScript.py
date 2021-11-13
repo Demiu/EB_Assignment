6e98b54408385541c597a7b82f6d47370051db79
grudos <jg04@wp.pl>
2021-11-13 20:56:28 +0100

    Adding ImportingScript and TestingScript 2

---
+++ scripts/importingScript/ImportingScript.py
@@ -0,0 +1,137 @@
+import os
+import random
+import time
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+
+os.environ['PATH'] += r'/home/grudos/Pulpit/Biz/scripts/seleniumDriver/linux/chromedriver'
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+admin_panel = "https://localhost/admin123456"
+login_admin = "prestashop@prestashop.pl"
+password_admin = "prestashop"
+
+categories_import_settings = ["truncate_1", "regenerate_0", "forceIDs_0", "sendemail_0"]
+products_import_settings = ["truncate_1", "match_ref_0", "regenerate_0", "forceIDs_0", "sendemail_0"]
+
+
+def loggingIn():
+    login_field = driver.find_element(By.CSS_SELECTOR, 'input[name="email"]')
+    login_field.clear()
+    login_field.send_keys(login_admin)
+
+    password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="passwd"]')
+    password_field.clear()
+    password_field.send_keys(password_admin)
+
+    driver.find_element(By.CSS_SELECTOR, 'button[id="submit_login"]').click()
+
+
+def goToTheCategoryPage():
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "subtab-AdminCategories")))
+
+    categories_page = driver.find_element(By.CSS_SELECTOR, 'li[id="subtab-AdminCategories"] a').get_attribute("href")
+    driver.get(categories_page)
+
+
+def importCategories():
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'a[id="category-grid-action-import"]')))
+
+    import_categories_page = driver.find_element(By.CSS_SELECTOR, 'a[id="category-grid-action-import"]').get_attribute("href")
+    driver.get(import_categories_page)
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'span[class="ps-switch"]')))
+
+    import_categories = Select(driver.find_element(By.CSS_SELECTOR, 'select[name="entity"]'))
+    import_categories.select_by_visible_text("Kategorie")
+
+    file_upload = driver.find_element(By.CSS_SELECTOR, 'input[id="file"]')
+    # Z WYKORZYSTANIEM os.getcwd() DO OBECNEJ SCIEZKI MOZNA COS TUTAJ WYKOMBINOWAC
+    file_upload.send_keys("/home/grudos/Pulpit/Biz/scripts/scraper/data/categories.csv")
+    time.sleep(1)
+
+    adjustSettings(categories_import_settings)
+    confirmImport()
+
+
+def importProducts():
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'select[name="entity"]')))
+
+    import_products = Select(driver.find_element(By.CSS_SELECTOR, 'select[name="entity"]'))
+    import_products.select_by_visible_text("Produkty")
+
+    file_upload = driver.find_element(By.CSS_SELECTOR, 'input[id="file"]')
+    # Z WYKORZYSTANIEM os.getcwd() DO OBECNEJ SCIEZKI MOZNA COS TUTAJ WYKOMBINOWAC
+    # PLIK NIZEJ JEST PRAWIDLOWY ALE DUZY TO NIE CHCE NA NIM TESTOWAC BO DLUGO BY TRWALO NA RAZIE
+    # file_upload.send_keys("/home/grudos/Pulpit/Biz/scripts/scraper/data/products.csv")
+    file_upload.send_keys("/home/grudos/Pulpit/Biz/scripts/scraper/data/product.csv")
+    time.sleep(1)
+
+    adjustSettings(products_import_settings)
+    confirmImport()
+
+
+def confirmImport():
+    driver.find_element(By.CSS_SELECTOR, 'button[name="submitImportFile"]').click()
+    driver.switch_to.alert.accept()
+
+    driver.find_element(By.CSS_SELECTOR, 'button[name="import"]').click()
+
+    # LINIE NIZEJ DO WYRZUCENIA JAK SIE SCRAPERA POPRAWI I NIE BEDZIE SIE CZEPIAL O PODATKI
+    try:
+        WebDriverWait(driver, 2).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[id="import_continue_button"]')))
+        driver.find_element(By.CSS_SELECTOR, 'button[id="import_continue_button"]').click()
+    except:
+        pass
+    # DOTAD TE LINIE
+
+    WebDriverWait(driver, 10000).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[id="import_close_button"]')))
+    driver.find_element(By.CSS_SELECTOR, 'button[id="import_close_button"]').click()
+
+
+def adjustSettings(import_settings):
+    for i in range(len(import_settings)):
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'input[id=' + import_settings[i] + ']').click()
+        except:
+            pass
+
+
+def settingIndexingProducts():
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'li[id="subtab-AdminParentSearchConf"]')))
+
+    search_preferences = driver.find_element(By.CSS_SELECTOR, 'li[id="subtab-AdminParentSearchConf"] a').get_attribute("href")
+    driver.get(search_preferences)
+
+    rebuild_index = driver.find_element(By.LINK_TEXT, "Przebuduj cały indeks")
+    rebuild_index.click()
+
+    announcement = driver.find_element(By.CSS_SELECTOR, 'div[class="alert alert-success"] button')
+    announcement.click()
+    time.sleep(1)
+
+
+
+
+driver.get(admin_panel)
+loggingIn()
+goToTheCategoryPage()
+importCategories()
+importProducts()
+settingIndexingProducts()
+driver.close()
---
+++ scripts/testingScript/TestingScript.py
@@ -0,0 +1,206 @@
+import os
+import random
+import time
+import string
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+os.environ['PATH'] += r'/home/grudos/Pulpit/Biz/scripts/seleniumDriver/linux/chromedriver'
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+added_lamps_count = 10
+categories_urls = ["https://localhost/pl/6-lampy-sufitowe-plafony", "https://localhost/pl/12-lampy-zewnetrzne-wiszace"]
+lamp_count_on_site = 12
+
+first_category_lamps_count = 5
+second_category_lamps_count = added_lamps_count - first_category_lamps_count
+
+ascii_letters = string.ascii_letters
+ascii_numbers = string.digits
+
+
+def generateRandomStrings(min_characters, max_characters):
+    string = ''
+    for _ in range(min_characters, max_characters):
+        string += random.choice(ascii_letters)
+    return string
+
+
+def generatePostcode():
+    postcode = ''
+    for _ in range(0, 2):
+        postcode += random.choice(ascii_numbers)
+    postcode += "-"
+    for _ in range(3, 6):
+        postcode += random.choice(ascii_numbers)
+    return postcode
+
+
+def addLampsToCart(category_url, category_lamps_count):
+    driver.get(category_url)
+    page = 1
+    added_lamp_num = 0
+    site_lamp_num = 0
+
+    while added_lamp_num < category_lamps_count:
+        if site_lamp_num == lamp_count_on_site:
+            page += 1
+            site_lamp_num = 0
+
+        if page == 1:
+            driver.get(category_url)
+        else:
+            driver.get(category_url+"?page="+str(page))
+
+        lamp = driver.find_elements(By.CSS_SELECTOR, 'a[class="thumbnail product-thumbnail"]')[site_lamp_num].get_attribute("href")
+        driver.get(lamp)
+
+        quantity = driver.find_element(By.CSS_SELECTOR, 'div[class="product-quantities"] span').get_attribute("data-stock")
+
+        # if there are 0 products in stock, it is not added
+        try:
+            amount = random.randint(1, int(quantity) - 1)
+            input_amount = driver.find_element(By.CSS_SELECTOR, 'div[class="qty"] input')
+            input_amount.send_keys(Keys.DELETE)
+            input_amount.send_keys(amount)
+
+            driver.find_element(By.CSS_SELECTOR, 'button[class$="add-to-cart"]').click()
+            added_lamp_num += 1
+        except:
+            pass
+
+        site_lamp_num += 1
+
+
+def deleteOneProductFromCart():
+    driver.get("https://localhost/pl/koszyk")
+
+    # MOZNA EWENTUALNIE ZROBIC ZEBY WYRZUCALO PRODUKT O LOSOWYM INDEKSIE A NIE ZAWSZE PIERWSZY
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, 'i[class="material-icons float-xs-left"]').click()
+
+
+def createAccount():
+    make_order = driver.find_element(By.CSS_SELECTOR, 'div[class="text-sm-center"] a').get_attribute("href")
+    driver.get(make_order)
+
+    gender = random.randint(0, 1)
+    driver.find_elements(By.CSS_SELECTOR, 'span[class="custom-radio"] input')[gender].click()
+
+    name = driver.find_element(By.CSS_SELECTOR, 'input[id="field-firstname"]')
+    created_name = generateRandomStrings(3, 15)
+    name.clear()
+    name.send_keys(created_name)
+
+    surname = driver.find_element(By.CSS_SELECTOR, 'input[id="field-lastname"]')
+    created_surname = generateRandomStrings(3, 15)
+    surname.clear()
+    surname.send_keys(created_surname)
+
+    email = driver.find_element(By.CSS_SELECTOR, 'input[id="field-email"]')
+    created_email = generateRandomStrings(3, 15) + "@gmail.com"
+    email.clear()
+    email.send_keys(created_email)
+
+    password = driver.find_element(By.CSS_SELECTOR, 'input[id="field-password"]')
+    created_password = generateRandomStrings(8, 15)
+    password.clear()
+    password.send_keys(created_password)
+
+    # processing of personal data
+    driver.find_element(By.CSS_SELECTOR, 'input[name="customer_privacy"]').click()
+    # agree to the terms and conditions
+    driver.find_element(By.CSS_SELECTOR, 'input[name="psgdpr"]').click()
+
+    driver.find_element(By.CSS_SELECTOR, 'button[name = "continue"]').click()
+
+    return created_email, created_password
+
+
+def completeAddresses():
+    address = driver.find_element(By.CSS_SELECTOR, 'input[id="field-address1"]')
+    created_address = generateRandomStrings(3, 15)
+    address.clear()
+    address.send_keys(created_address)
+
+    city = driver.find_element(By.CSS_SELECTOR, 'input[id="field-city"]')
+    created_city = generateRandomStrings(3, 15)
+    city.clear()
+    city.send_keys(created_city)
+
+    postcode = driver.find_element(By.CSS_SELECTOR, 'input[id="field-postcode"]')
+    created_postcode = generatePostcode()
+    postcode.clear()
+    postcode.send_keys(created_postcode)
+
+    country = Select(driver.find_element(By.CSS_SELECTOR, 'select[name="id_country"]'))
+    country.select_by_visible_text("Polska")
+
+    driver.find_element(By.CSS_SELECTOR, 'button[name="confirm-addresses"]').click()
+
+
+def chooseDelivery():
+    carrier = random.randint(0, 1)
+    try:
+        driver.find_elements(By.CSS_SELECTOR, 'div[class="col-sm-1"] input')[carrier].click()
+    except:
+        pass
+    driver.find_element(By.CSS_SELECTOR, 'button[name="confirmDeliveryOption"]').click()
+
+
+def choosePayment():
+    # BYC MOZE DO ZMIANY BO JAK TO ROBILEM TO BYLO JESZCZE PLACENIE CZEKAMI, TUTAJ JEST USTAWIONE W SKRYPCIE
+    # ZEBY BYLO WYBRANE PLACENIE PRZELEWEM
+    try:
+        driver.find_elements(By.CSS_SELECTOR, 'div[class="payment-option clearfix"] input')[1].click()
+    except:
+        pass
+
+    # agree to the terms of service
+    driver.find_element(By.CSS_SELECTOR, 'input[id="conditions_to_approve[terms-and-conditions]"]').click()
+    # submit button
+    driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-primary center-block"]').click()
+
+
+def checkOrderStatus(email, password):
+    my_account = driver.find_element(By.CSS_SELECTOR, 'a[class="account"]').get_attribute("href")
+    driver.get(my_account)
+
+    # sometimes you had to log into your account
+    try:
+        login_field = driver.find_element(By.CSS_SELECTOR, 'input[id="field-email"]')
+        login_field.clear()
+        login_field.send_keys(email)
+
+        password_field = driver.find_element(By.CSS_SELECTOR, 'input[id="field-password"]')
+        password_field.clear()
+        password_field.send_keys(password)
+
+        # logging in to the account
+        driver.find_element(By.CSS_SELECTOR, 'button[id="submit-login"]').click()
+    except:
+        pass
+
+    # history and details of the order
+    order_histories = driver.find_element(By.CSS_SELECTOR, 'a[id="history-link"]').get_attribute("href")
+    driver.get(order_histories)
+
+    order_status = driver.find_element(By.CSS_SELECTOR, 'a[data-link-action="view-order-details"]').get_attribute("href")
+    driver.get(order_status)
+
+
+
+
+addLampsToCart(categories_urls[0], first_category_lamps_count)
+addLampsToCart(categories_urls[1], second_category_lamps_count)
+
+deleteOneProductFromCart()
+email_account, password_account = createAccount()
+completeAddresses()
+chooseDelivery()
+choosePayment()
+checkOrderStatus(email_account, password_account)
