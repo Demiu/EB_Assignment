2352d415c1076199483dffdfdd9dadedb6e94964
grudos <jg04@wp.pl>
2021-11-22 17:08:52 +0100

    Update HowToRun.md

--- install-chromedriver.sh
+++ install-chromedriver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -P ./scripts/seleniumDriver/linux https://chromedriver.storage.googleapis.com/95.0.4638.69/chromedriver_linux64.zip
+wget -P ./scripts/seleniumDriver/linux https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip
 unzip ./scripts/seleniumDriver/linux/chromedriver_linux64.zip -d ./scripts/seleniumDriver/linux/
