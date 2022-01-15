ccbaa3eda74800e651130c340f862580406476f0
Demiu <miazgapow@gmail.com>
2022-01-15 14:19:44 +0100

    Database deploy - cleanup in container

--- scripts/database_deploy/in_cluster/in_db_container/deploy_in_container.sh
+++ scripts/database_deploy/in_cluster/in_db_container/deploy_in_container.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Creating user and database"
 mysql -pstudent < /tmp/be_171974/make_database.sql
+echo "Loading backup..."
 mysql -uBE_171974 -ppassword BE_171974_DB < /tmp/be_171974/backup.sql
+
+echo "Cleaning up files..."
+rm -rf /tmp/be_171974
 
